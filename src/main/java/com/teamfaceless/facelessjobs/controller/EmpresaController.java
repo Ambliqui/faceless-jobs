642feb0fb948811e6e5a065105e2c31179138e97
Ambliqui <isiseren@gmail.com>
2022-01-25 21:20:31 +0100

    Update EmpresaController.java

--- src/main/java/com/teamfaceless/facelessjobs/controller/EmpresaController.java
+++ src/main/java/com/teamfaceless/facelessjobs/controller/EmpresaController.java
@@ -87,7 +87,8 @@ public class EmpresaController {
 	
 	@GetMapping("/modificar/{idEmpresa}")
 	public String goModificar(@PathVariable Integer idEmpresa, Model model) {
-		model.addAttribute("empresa", iEmpresaService.findById(idEmpresa));
+		Empresa empresa = iEmpresaService.findById(idEmpresa).get();
+		model.addAttribute("empresa", empresa);
 		model.addAttribute("empresaregistrodto", new EmpresaRegistroDto());
 		model.addAttribute("provincias", iProvinciaService.findAll());
 		model.addAttribute("sectores", iSectorService.findAll());
